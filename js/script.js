a60ee00998de763e5940949773f0701d668d9cbc
alyssia-fung <124389558+alyssia-fung@users.noreply.github.com>
2023-05-01 23:34:43 +0000

    fixed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Alyssia fung All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Alyssia fung
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculate() {
+  const age = document.getElementById("age").value
+
+  if (age >= 18) {
+    document.getElementById("answer").innerHTML =
+      "You can see G, PG and R rated movies!"
+  } else if (age >= 13) {
+    document.getElementById("answer").innerHTML =
+      "You can see G and PG rated movies!"
+  } else if (age >= 5) {
+    document.getElementById("answer").innerHTML = "You can see G rated movies!"
+  } else {
+    document.getElementById("answer").innerHTML =
+      "you can hardly watch any movies"
+  }
+  console.log("Thanks for verifying you're age!")
+}
